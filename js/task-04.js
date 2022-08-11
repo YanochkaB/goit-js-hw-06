b7260b7401134fbd559848d43c27fefea2b4f929
YanochkaB <yanabukhtij@gmail.com>
2022-08-11 22:57:24 +0300

    4 задача

---
+++ js/task-04.js
@@ -0,0 +1,31 @@
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+//Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+//Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = Number(document.querySelector("#value").textContent);
+console.log(counterValue)
+
+let chislo = document.querySelector("#value");
+console.log(`число`, chislo)
+
+const butDecrement = document.querySelector("#counter").firstElementChild;
+const butIncement = document.querySelector("#counter").lastElementChild;
+
+const butClick = () => {
+    console.log('Ура +1')
+    counterValue += 1;
+    chislo.innerHTML = counterValue;
+}
+
+butIncement.addEventListener("click", butClick);
+
+const butNotClick = () => {
+    console.log('мінус')
+    counterValue -= 1;
+    chislo.innerHTML = counterValue;
+}
+
+butDecrement.addEventListener("click", butNotClick);
+
+// chislo.innerHTML = counterValue;
+// console.log('chislo', chislo)

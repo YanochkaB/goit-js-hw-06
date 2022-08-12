efe01da6bd2773776b426b21b0e00d5f951792a6
YanochkaB <yanabukhtij@gmail.com>
2022-08-12 23:15:31 +0300

    8 готова

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,10 @@
-// const categories = document.querySelector('#categories');
+const categories = document.querySelector('#categories');
 
-// console.log(`Number of categories: ${categories.children.length}`);
+console.log(`Number of categories: ${categories.children.length}`);
 
-// const categoreItems = categories.querySelectorAll('li.item');
+const categoreItems = categories.querySelectorAll('li.item');
 
-// categoreItems.forEach(item => {
-//     console.log(`Category: ${item.querySelector("h2").textContent}`);
-//     console.log(`Elements: ${item.querySelectorAll('li').length}`);
-// })
+categoreItems.forEach(item => {
+    console.log(`Category: ${item.querySelector("h2").textContent}`);
+    console.log(`Elements: ${item.querySelectorAll('li').length}`);
+})
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+//Напиши скрипт управління формою логіна.
+//Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+//Під час відправлення форми сторінка не повинна перезавантажуватися.       
+//Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+//Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, 
+//де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості.
+//Для доступу до елементів форми використовуй властивість elements.
+//Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
+
+//preventDefault();
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: {email, password},
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('Заповніть всі поля')
+    }
+    const user = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(`email: ${email.value}`)
+    event.currentTarget.reset()
+}

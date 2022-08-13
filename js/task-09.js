970ff13aeff885a093ba6603100a61cc959f380f
YanochkaB <yanabukhtij@gmail.com>
2022-08-13 09:10:56 +0300

    9 готово (здаю)

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
+const button = document.querySelector(".change-color");
+// console.log(button)
+
+const colorBody = document.querySelector(".color");
+// console.log(colorBody)
+
+const body = document.querySelector('body')
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+button.addEventListener('click', (event) => {
+  // console.log('зміна кольору')
+  let color = getRandomHexColor(); // визначити колір
+  body.style.backgroundColor = color; // змінити колір
+  colorBody.textContent = color; // записати колір
+})
